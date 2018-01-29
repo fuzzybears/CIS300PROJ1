76f036a4bdea88e9c8de569eed74c18aa5dc9677
rhowell <rhowell@ksu.edu>
2018-01-29 16:18:51 -0600

    Lab 8 start.

---
+++ Ksu.Cis300.CapitalGainCalculator/UserInterface.cs
@@ -0,0 +1,29 @@
+﻿/* UserInterface.cs
+ * Author: Rod Howell
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ksu.Cis300.CapitalGainCalculator
+{
+    /// <summary>
+    /// A user interface for a simple captial gain calculator for a single stock commodity.
+    /// </summary>
+    public partial class UserInterface : Form
+    {
+        /// <summary>
+        /// Constructs the GUI.
+        /// </summary>
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
